c762a572b2362e9a28bbfb549fdcea6685c8e1dd
Adam Wyatt <adameagle20@hotmail.com>
2022-05-06 08:36:59 -0400

    Inject http client

--- DissidiaWebUI/Pages/Character/DisplayCharacter.cs
+++ DissidiaWebUI/Pages/Character/DisplayCharacter.cs
@@ -1,6 +1,12 @@
-﻿namespace DissidiaWebUI.Pages.Character
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DissidiaWebUI.Pages.Character
 {
+
     public partial class DisplayCharacter
     {
+        [Inject]
+        private IHttpClientFactory? _httpClientFactory { get; set; }
+        private string? _errorString;
     }
 }
