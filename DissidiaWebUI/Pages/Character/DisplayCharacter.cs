dd6528c28e5f4722182b45b939a25dbee426c9ae
Adam Wyatt <adameagle20@hotmail.com>
2022-05-06 20:49:19 -0400

    Add UI Models and begin display character page.

--- DissidiaWebUI/Pages/Character/DisplayCharacter.cs
+++ DissidiaWebUI/Pages/Character/DisplayCharacter.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using DissidiaWebUI.Models;
+using Microsoft.AspNetCore.Components;
 
 namespace DissidiaWebUI.Pages.Character
 {
 
     public partial class DisplayCharacter
     {
+        [Parameter]
+        public string CharacterId { get; set; }
         [Inject]
         private IHttpClientFactory? _httpClientFactory { get; set; }
+        private CharacterModel _characterModel { get; set; }
         private string? _errorString;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            try
+            {
+                _characterModel = await client.GetFromJsonAsync<CharacterModel>($"https://localhost7172/api/character/{CharacterId}");
+            }
+            catch (Exception ex)
+            {
+                _errorString = ex.Message;
+            }
+
+            //return base.OnInitializedAsync();
+        }
     }
 }
