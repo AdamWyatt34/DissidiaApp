29edee5a74f9719fd42d86d93ee5b7fd0e65d8cb
Adam Wyatt <adameagle20@hotmail.com>
2022-05-14 23:53:54 -0400

    Configure Dropdown Component and Update UI

--- DissidiaWebUI/Pages/Character/Attack.cs
+++ DissidiaWebUI/Pages/Character/Attack.cs
@@ -11,10 +11,17 @@ namespace DissidiaWebUI.Pages.Character
         public AttackModel RenderAttack { get; set; } = new AttackModel();
 
         private List<PriorityType> priorityValues = new List<PriorityType>();
+        private string edit = "Edit";
+        private string delete = "Delete";
 
         public Attack()
         {
             priorityValues = Enum.GetValues(typeof(PriorityType)).Cast<PriorityType>().ToList();
+        }
+
+        public void HandleOnSelected(string selected)
+        {
+            Console.WriteLine(selected);
         }
 
     }
